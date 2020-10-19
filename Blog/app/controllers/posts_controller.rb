bc8ffec3814412c46fee624965958e3f47eb74e6
SimonHe <siminghe2020@gmail.com>
2020-10-20 00:32:44 +0800

    final refine

--- Blog/app/controllers/posts_controller.rb
+++ Blog/app/controllers/posts_controller.rb
@@ -2,30 +2,46 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
-    (@post.save) ? (redirect_to post_path(@post)): (render 'new')
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      (render 'new')
+    end
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   def edit
     @post = Post.find(params[:id])
   end
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    (@post.update(post_params)) ? (redirect_to post_path(@post)): (render "/posts/#{@post.id}/edit")
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render "/posts/#{@post.id}/edit"
+    end
   end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to "/posts"
+    redirect_to '/posts'
   end
+
   private
+
   def post_params
     params.require(:post).permit(:title, :content)
   end
