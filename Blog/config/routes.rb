d68a93ef6d67bab93fc1cd0826859bcc99c3281c
SimonHe <siminghe2020@gmail.com>
2020-10-20 18:39:24 +0800

    root route

--- Blog/config/routes.rb
+++ Blog/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'pages#home'
+  #get '/', to: 'pages#home'
+  root to: "pages#home"
   get '/posts', to: 'posts#index'
   get '/posts/new', to: 'posts#new'
   post '/posts', to: 'posts#create'
